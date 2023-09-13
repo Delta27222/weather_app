b86bfcfd286efac8650ac11fc24e7ad008a6c078
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-13 15:59:14 -0700

    refactor(Refactor): :recycle: Refactor some imports

--- src/store/slices/dataSlice.js
+++ src/store/slices/dataSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getFullWeather, getSimpleWeather } from '../../component';
+import { getFullWeather, getSimpleWeather } from '../../api';
 
 
 const initialState ={
