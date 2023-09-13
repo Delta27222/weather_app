9c5fde9664d3da9b2890ce7528cf5879c4e56eb3
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-13 15:57:12 -0700

    ci(ConfigFile): :wrench: Add color to tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dim-white': '#EFF2FB',
+        'dim-card' : '#FEFEFF',
+        'dim-selected-mini-card': '#42B1FA',
+        'dim-unselected-mini-card': '#FAFAFA',
+      },
+    },
   },
   plugins: [],
 }
