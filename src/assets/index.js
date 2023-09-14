6b0461567e0b8c3a341919d2cb7aa4c16a8f520a
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-13 20:50:35 -0700

    feat(Assets): :bento: Add new Assets

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import searchIcon from "./icons/searchIcon.svg"
+import refreshIcon from "./icons/refreshIcon.svg"
+import locationIcon from "./icons/locationIcon.svg"
+
+import DaySun from "./weatherIcons/DaySun.svg"
+import DayClouds from "./weatherIcons/DayClouds.svg"
+import DayWind from "./weatherIcons/DayWind.svg"
+import DayRain from "./weatherIcons/DayRain.svg"
+import DaySnow from "./weatherIcons/DaySnow.svg"
+import DayStorm from "./weatherIcons/DayStorm.svg"
+import NightMoon from "./weatherIcons/NightMoon.svg"
+
+export { searchIcon, refreshIcon, locationIcon, DaySun, DayClouds, DayWind, DayRain, DaySnow, DayStorm, NightMoon}
