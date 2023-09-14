3f6139d7bdcf2e911fa2fedb7d70fdd7d671dc20
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-13 20:52:30 -0700

    feat(NewComponent): :sparkles: Add new components

---
+++ src/component/index.js
@@ -0,0 +1,10 @@
+import SearchBoxComponent from "./searchBoxComponent/searchBox.componen";
+import CardWeatherComponent from "./cardWeatherComponent/cardWeather.component";
+import  HeaderSectionComponent from "./headerSectionComponent/headerSection.component";
+import MidInfoComponent from "./midInfoComponent/midInfo.component";
+export {
+  SearchBoxComponent,
+  CardWeatherComponent,
+  HeaderSectionComponent,
+  MidInfoComponent
+}
