5cbb65b2f755f7a8a57617237143212dd7716ef5
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-20 11:47:15 -0700

    refactor(Refactor): :recycle: Removing function to select the icon

--- src/component/midInfoComponent/midInfo.component.jsx
+++ src/component/midInfoComponent/midInfo.component.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useSelector } from 'react-redux';
-import { DayClouds, DayRain, DaySnow, DayStorm, DaySun, NightMoon } from '../../assets';
+import { NightMoon } from '../../assets';
+import { imageWeather } from "../../hooks/searchIcon";
 
 const MidInfoComponent = ({loading}) => {
   const info = useSelector(state => state.data.simpleWeatherInfo)
@@ -22,19 +23,4 @@ export default MidInfoComponent
 
 function capitalizeString(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
-}
-
-function imageWeather(idWeather) {
-  if (idWeather >= 200 && idWeather <= 232) {
-    return DayStorm;
-  } else if ((idWeather >= 300 && idWeather <= 321) || (idWeather >= 500 && idWeather <= 531)) {
-    return DayRain;
-  } else if (idWeather >= 600 && idWeather <= 622) {
-    return DaySnow;
-  } else if (idWeather >= 801 && idWeather <= 804) {
-    return DayClouds;
-  } else if (idWeather === 800) {
-    return DaySun;
-  }
-  return NightMoon;
 }
