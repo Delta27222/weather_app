8ba94e7cb63e152a481d072997e404a3386ab6dd
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-19 20:13:49 -0700

    feat(New Component): :sparkles: New component with info

--- src/component/index.js
+++ src/component/index.js
@@ -2,9 +2,12 @@ import SearchBoxComponent from "./searchBoxComponent/searchBox.componen";
 import CardWeatherComponent from "./cardWeatherComponent/cardWeather.component";
 import  HeaderSectionComponent from "./headerSectionComponent/headerSection.component";
 import MidInfoComponent from "./midInfoComponent/midInfo.component";
+import InfoWindRainHumComponent from "./infoWindRainHumComponent/infoWindRainHum.component";
+
 export {
   SearchBoxComponent,
   CardWeatherComponent,
   HeaderSectionComponent,
-  MidInfoComponent
+  MidInfoComponent,
+  InfoWindRainHumComponent
 }
---
+++ src/component/infoWindRainHumComponent/infoWindRainHum.component.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import { useSelector } from "react-redux"
+import { drop, rain, wind } from "../../assets"
+import { useEffect } from "react"
+
+const InfoWindRainHumComponent = () => {
+  const info = useSelector(state => state.data.fullWeatherInfo)
+  var firstObj = {}
+  var allInfo = []
+
+  if (info && info.list && info.list.length > 0) {
+    firstObj = info.list[0];
+    allInfo = [
+      {
+        icon: wind,
+        value: firstObj.wind.speed,
+        unit: 'km/h',
+        title: 'Wind'
+      },
+      {
+        icon: drop,
+        value: firstObj.main.humidity,
+        unit: '%',
+        title: 'Humidity'
+      },
+      {
+        icon: rain,
+        value: firstObj.rain['3h'],
+        unit: '%',
+        title: 'Rain'
+      }
+    ]
+  }
+  console.log(allInfo)
+
+
+  return (
+    <section className="flex flex-row justify-center items-center gap-1 mt-4">
+      {allInfo.map((item, index) => ((
+        <div key={index} className="w-[80px] h-[50px] py-4 flex flex-col justify-center items-center rounded-2xl space-y-1 shadow-md">
+          <img src={item.icon} alt="icon" className="w-3"/>
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-[8px] font-bold">{`${item.value} ${item.unit}`}</p>
+            <p className="text-[10px] text-gray-400 font-thin ">{item.title}</p>
+          </div>
+        </div>
+      )))}
+    </section>
+  )
+}
+
+export default InfoWindRainHumComponent
