b4a710fa42742110ce75e3d342d0189b08787ff9
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-20 15:36:10 -0700

    feat(Toaster): :sparkles: Add toaster component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { Toaster } from 'react-hot-toast'
 import './App.css'
 import { CardWeatherComponent } from './component'
 
 function App() {
   return (
     <main className='flex flex-col justify-center items-center h-screen w-screen bg-dim-white'>
-        {/* <h1 className='text-4xl font-bold'>Weather App</h1> */}
         <CardWeatherComponent />
+        <Toaster />
     </main>
   )
 }
