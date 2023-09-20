3c3b909510743e489cc37d73d0b30c989b5fa41b
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-20 16:06:31 -0700

    refactor(Refactor): :recycle: refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import { Toaster } from 'react-hot-toast'
@@ -8,6 +9,10 @@ function App() {
   return (
     <main className='flex flex-col justify-center items-center h-screen w-screen bg-dim-white'>
         <CardWeatherComponent />
+        <section className='w-full bottom-0 sticky flex flex-row justify-center items-center mt-5 text-black'>
+          <p>Designed by <a href="https://dribbble.com/Offdesignarea" target='_blank' className='hover:text-dim-selected-mini-card' >Offdesignarea</a>
+          , Built by <a href="https://angel-hernandez-dev.vercel.app" target='_blank' className='hover:text-dim-selected-mini-card' >Angel G. Hernandez</a></p>
+          </section>
         <Toaster />
     </main>
   )
