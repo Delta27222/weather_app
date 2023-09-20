6c4cdc93a0dca80e894631307574e28a879ae4eb
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-20 13:43:08 -0700

    feat(New url fetchs): :sparkles: New url to fetch data

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,22 @@
+export const getSimpleWeatherLatLog = (lat, lon) => {
+  return fetch(`${import.meta.env.VITE_BASEURL}lat=${lat}&lon=${lon}${import.meta.env.VITE_APIKEY}`)
+  .then(res => res.json())
+  .catch(error => console.log(error))
+}
+export const getFullWeatherLatLon = (lat,lon) => {
+  return fetch(`${import.meta.env.VITE_BASEURL2}lat=${lat}&lon=${lon}${import.meta.env.VITE_APIKEY}`)
+  .then(res => res.json())
+  .catch(error => console.log(error))
+}
 
 export const getSimpleWeather = (city) => {
-  return fetch(`${import.meta.env.VITE_BASEURL}${city}${import.meta.env.VITE_APIKEY}`)
+  return fetch(`${import.meta.env.VITE_BASEURL}q=${city}${import.meta.env.VITE_APIKEY}`)
   .then(res => res.json())
   .catch(error => console.log(error))
 }
 
 export const getFullWeather = (city) => {
-  return fetch(`${import.meta.env.VITE_BASEURL2}${city}${import.meta.env.VITE_APIKEY}`)
+  return fetch(`${import.meta.env.VITE_BASEURL2}q=${city}${import.meta.env.VITE_APIKEY}`)
   .then(res => res.json())
   .catch(error => console.log(error))
 }
